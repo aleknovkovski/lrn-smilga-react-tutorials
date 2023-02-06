fb1d98e2b3c7fa97e94684cf84646bcf37832479
Alek Novkovski <alek@greatcode2.com>
2023-02-06 19:01:30 +0100

    01-2: Complete lesson code, basic counter w useState

--- src/tutorial/01-useState/working/02-useState-basics.jsx
+++ src/tutorial/01-useState/working/02-useState-basics.jsx
@@ -1,5 +1,18 @@
+import { useState } from 'react';
+
 const UseStateBasics = () => {
-  return <h2>useState basics</h2>;
+  const [count, setCount] = useState(0);
+  const handleClick = () => {
+    setCount(count + 1);
+  };
+  return (
+      <div>
+        <h4>You clicked {count} times</h4>
+        <button className='btn' onClick={handleClick}>
+          Click me
+        </button>
+      </div>
+  );
 };
 
 export default UseStateBasics;
