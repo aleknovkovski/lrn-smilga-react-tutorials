5bc796a0bd275b760c14ec03633162b4040702e6
Alek Novkovski <alek@greatcode2.com>
2023-02-06 21:37:14 +0100

    01-3: Finish exercise; An example of useState w array

--- src/tutorial/01-useState/working/03-useState-array.jsx
+++ src/tutorial/01-useState/working/03-useState-array.jsx
@@ -1,5 +1,38 @@
+import React, {useState} from 'react';
+import { data } from '../../../data';
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+  const [people, setPeople] = useState(data);
+
+  const removePerson = (id) => {
+    const newPeopleArr = people.filter((person) => {
+      return person.id !== id
+    });
+    setPeople(newPeopleArr);
+  };
+
+  function clearPeopleList () {
+    setPeople([]);
+  }
+
+  return (
+      <div>
+        {people.map((person) => {
+          const { id, name } = person;
+          return (
+              <div key={id} className='item'>
+                <h4>{name}</h4>
+                <button onClick={() => removePerson(id)}>remove</button>
+              </div>
+          );
+        })}
+        <button
+            className='btn'
+            onClick={clearPeopleList}
+        >
+          clear items
+        </button>
+      </div>
+  );
 };
 
 export default UseStateArray;
