d6311feb6471468145c66581ebdf3119aa5a3373
Alek Novkovski <alek@greatcode2.com>
2023-02-07 23:22:18 +0100

    01-4a: Finish exercise; Shows need for obj in useState

--- src/tutorial/01-useState/working/04-useState-object.jsx
+++ src/tutorial/01-useState/working/04-useState-object.jsx
@@ -1,7 +1,25 @@
 import { useState } from 'react';
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+    const [name, setName] = useState('peter');
+    const [age, setAge] = useState(24);
+    const [hobby, setHobby] = useState('read books');
+
+    const displayPerson = () => {
+        setName('john');
+        setAge(28);
+        setHobby('scream at the computer');
+    };
+    return (
+        <>
+            <h3>{name}</h3>
+            <h3>{age}</h3>
+            <h4>Enjoys To: {hobby}</h4>
+            <button className='btn' onClick={displayPerson}>
+                show john
+            </button>
+        </>
+    );
 };
 
 export default UseStateObject;
