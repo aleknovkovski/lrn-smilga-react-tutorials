752dbd16973383da8fd7ef3d98637a3c86d498d6
Alek Novkovski <alek@greatcode2.com>
2023-02-08 02:05:46 +0100

    01-5a: Finish exercise; Shows async nature of setState

--- src/tutorial/01-useState/working/05-useState-gotcha.jsx
+++ src/tutorial/01-useState/working/05-useState-gotcha.jsx
@@ -1,5 +1,23 @@
+import { useState } from 'react';
+
 const UseStateGotcha = () => {
-  return <h2>useState "gotcha"</h2>;
+    const [value, setValue] = useState(0);
+
+    const handleClick = () => {
+        console.log('clicked the button');
+        console.log("inside handler, before setState:" + value)
+        setValue(value + 1);
+        console.log("inside handler, after setState:" + value)
+    }
+    console.log("in render:" + value)
+    return (
+        <div>
+            <h1>{value}</h1>
+            <button className='btn' onClick={handleClick}>
+                increase
+            </button>
+        </div>
+    );
 };
 
 export default UseStateGotcha;
